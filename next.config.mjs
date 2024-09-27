b7309f0490df837e005e885a77da148fa52b447d
CrCardd <cris,rc377@gmail.com>
2024-09-27 17:00:15 -0300

    mini calculadora feia e mal feita

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    rewrites: () => {
+        return[
+            {
+                source: "/",
+                destination: "/home"
+                
+            },
+            {
+                source: "/maths",
+                destination: "/maths"
+
+            },
+        ]
+    }
+};
 
 export default nextConfig;
--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,12 +1,13 @@
+import { ROUTES } from '@/constants/routes';
 import Link from 'next/Link';
 
 
 export const Menu = () => {
     return(
         <>
-            <nav className='fixed w-screen bg-gray-700 p-2 gap-x-5 flex justify-center align-items flex-row font-karlafont '>
-                <p className='bg-gray-300 border-8 rounded cursor-pointer' >Menu</p>
-                <p className='bg-gray-300 border-8 rounded cursor-pointer' >Home</p>
+            <nav className=' w-screen bg-gray-700 p-2 gap-x-5 flex justify-center align-items flex-row font-karlafont '>
+                <Link href={ROUTES.home} className='bg-gray-300 border-8 rounded cursor-pointer' >Menu</Link>
+                <Link href={ROUTES.maths} className='bg-gray-300 border-8 rounded cursor-pointer' >Home</Link>
             </nav>
         </>
     )
