0bacb3949d313e71a3d325daffefcdd76733ef44
CrCardd <cris.rc377@gmail.com>
2024-10-03 16:43:56 -0300

    finalizando

--- src/app/catsPage/page.tsx
+++ src/app/catsPage/page.tsx
@@ -5,10 +5,10 @@ const cats: React.FC = () => {
 
     return(
         <>
-            <div className="bg-gradient-to-r from-gray-900 via-gray-700 to-gray-900 w-full min-h-screen h-full flex justify-evenly items-center flex-wrap p-[100px]">
-                <Cat_card name={"BEBÊ"} age={"2016 | 2024"} color={"amber"}/>
-                <Cat_card name={"LUA"} age={"2020 | 2021"} color={"indigo"}/>
-                <Cat_card name={"MARIE"} age={"2018 | 2019"} color={"red"}/>
+            <div className="bg-gradient-to-r from-gray-900 via-gray-700 to-gray-900 w-full min-h-screen h-full flex justify-center gap-[50px] items-center flex-wrap p-[100px]">
+                <Cat_card name={"BEBÊ"} age={"2016 | 2024"}  dividcolor={"divide-amber-950"} textcolor={"text-amber-500"}    backcolor={"bg-amber-800"}     bordercolor={"border-amber-950"}/>
+                <Cat_card name={"LUA"} age={"2020 | 2021"}   dividcolor={"divide-indigo-950"} textcolor={"text-indigo-500"}   backcolor={"bg-indigo-800"}    bordercolor={"border-indigo-950"}/>
+                <Cat_card name={"MARIE"} age={"2018 | 2019"} dividcolor={"divide-red-950"} textcolor={"text-red-500"}      backcolor={"bg-red-800"}       bordercolor={"border-red-950"}/>
             </div>
         </>
     )
--- src/components/cat_card.tsx
+++ src/components/cat_card.tsx
@@ -5,25 +5,25 @@ import lua from '@/app/assets/lua.png'
 import bebe from '@/app/assets/bebe.png'
 
 
-const Cat_card = ({name, color, age}: {name: string, color: string, age: string}) => {
+const Cat_card = ({name, textcolor, backcolor, bordercolor, dividcolor, age}: {name: string, textcolor: string, backcolor: string, dividcolor: string, bordercolor: string, age: string}) => {
 
     return(
         <>  
-            <div className="rounded-[35px] w-[400px] h-[600px] bg-gray-200 hover:scale-105">
+            <div className="rounded-[35px] w-[400px] h-[600px] bg-gray-200 hover:scale-110 transition-transform duration-150">
                 <Image className="w-full h-[250px] rounded-t-[30px]" src={name === "MARIE" ? marie : name === "LUA" ? lua : bebe} alt='gatuno'/>
                 <div className="p-4 flex flex-col items-center justify-around h-[250px]">
-                    <p className={`text-[14px] text-${color}-800`}>⭐ {age}  ✝</p>
+                    <p className={`text-[14px] ${textcolor}`}>⭐ {age}  ✝</p>
                     <strong className="text-[30px]">{name}</strong>
                     <p>😺</p>
                 </div>
-                <div className={`rounded-b-[30px] h-[100px] w-full bg-${color}-700 flex`}>
-                    <div className={`border-t-[3px] h-full w-[133px] border-${color}-950`}>
+                <div className={`rounded-b-[30px] h-[100px] w-full ${backcolor} flex divide-x ${dividcolor}`}>
+                    <div className={`border-t-[3px] h-full w-[133px] ${bordercolor}`}>
 
                     </div>
-                    <div className={`border-r-[3px] border-l-[3px] border-t-[3px] h-full w-[133px] border-${color}-950`}>
+                    <div className={`border-t-[3px] h-full w-[133px] ${bordercolor}`}>
 
                     </div>
-                    <div className={`border-t-[3px] h-full w-[133px] border-${color}-950`}>
+                    <div className={`border-t-[3px] h-full w-[133px] ${bordercolor}`}>
 
                     </div>
                 </div>
